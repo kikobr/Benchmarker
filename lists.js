43ff697e13d84d3a98b619490d50c85a40af43fe
Kiko Herrschaft <kiko.herrschaft@gmail.com>
2020-08-12 23:27:13 -0300

    fix

--- lists.js
+++ lists.js
@@ -1,16 +1,15 @@
 module.exports = {
-    ciaBr: [
-        "latam linhas aéreas",
-        "azul linhas aéreas",
-        "gol linhas aéreas",
-    ],
-    ciaEn: [
-        "united airlines",
-        "delta airlines",
-        "british airways",
-        "jetblue airlines",
-        // "air asia airlines",
-        "volaris airlines",
-        "american airlines",
-    ]
+	"ciaBr": [
+		"latam linhas aéreas",
+		"azul linhas aéreas",
+		"gol linhas aéreas"
+	],
+	"ciaEn": [
+		"united airlines",
+		"delta airlines",
+		"british airways",
+		"jetblue airlines",
+		"volaris airlines",
+		"american airlines"
+	],
 }
